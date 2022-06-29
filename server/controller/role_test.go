b383884cbe3f31a4bbcb15ac4ea88f26b65ea522
Shounak Mulay <shounakmulay@gmail.com>
2022-06-29 20:11:35 +0530

    Add more role controller test

--- server/controller/role_test.go
+++ server/controller/role_test.go
@@ -1,11 +1,11 @@
 package controller_test
 
 import (
-	// "errors"
 	"errors"
 	"go-todo/server/controller"
 	"go-todo/server/model/dbmodel"
 	"go-todo/server/model/reqmodel"
+	"go-todo/server/model/resmodel"
 	"go-todo/test/mocks"
 	"testing"
 
@@ -29,7 +29,6 @@ func (r *RoleControllerTestSuite) SetupTest() {
 }
 
 func (r *RoleControllerTestSuite) Test_CreateRole() {
-
 	tests := []struct {
 		TestName    string
 		Name        string
@@ -77,5 +76,58 @@ func (r *RoleControllerTestSuite) Test_CreateRole() {
 		})
 	}
 	r.mockRoleDao.AssertNumberOfCalls(r.T(), "CreateRole", 2)
+	r.mockRoleDao.AssertExpectations(r.T())
+}
+
+func (r *RoleControllerTestSuite) Test_FindRoleByID() {
+	tests := []struct {
+		TestName        string
+		RoleID          int
+		RoleName        string
+		RoleAccessLevel int
+		returnErr       error
+	}{
+		{
+			TestName:        "returns role res model",
+			RoleID:          10,
+			RoleName:        "user",
+			RoleAccessLevel: 100,
+			returnErr:       nil,
+		},
+		{
+			TestName:        "returns error from role dao",
+			RoleID:          20,
+			RoleName:        "admin",
+			RoleAccessLevel: 200,
+			returnErr:       errors.New("test err: error finding role"),
+		},
+	}
+
+	for _, t := range tests {
+		r.Run(t.TestName, func() {
+			mockDbRole := dbmodel.Role{
+				ID:          t.RoleID,
+				Name:        t.RoleName,
+				AccessLevel: t.RoleAccessLevel,
+			}
+			r.mockRoleDao.On("FindRoleByID", t.RoleID).Return(mockDbRole, t.returnErr)
+
+			role, err := r.roleController.FindRoleByID(t.RoleID)
+
+			if t.returnErr == nil {
+				require.NoError(r.T(), err)
+			} else {
+				require.Error(r.T(), err)
+			}
+
+			expectedRole := resmodel.Role{
+				ID:          t.RoleID,
+				Name:        t.RoleName,
+				AccessLevel: t.RoleAccessLevel,
+			}
+			require.Equal(r.T(), expectedRole, role)
+		})
+	}
+	r.mockRoleDao.AssertNumberOfCalls(r.T(), "FindRoleByID", 2)
 	r.mockRoleDao.AssertExpectations(r.T())
 }
